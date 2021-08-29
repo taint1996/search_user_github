623efeb8e7d58546f7186b6510f85110d500dd87
Nguyen Trung Tai (TJ) <tai.nguyentrung1996@gmail.com>
2021-08-29 23:13:52 +0700

    Done search github user, get user, get user repos, about page.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     state = {
         users: [],
         user: {},
+        repos: [],
         loading: false,
         alert: null
     }
@@ -69,9 +70,26 @@ class App extends Component {
         })
     }
 
+    // get users repos
+    getUserRepos = async(username) => {
+        this.setState({
+            loading: true
+        });
+
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`) // ?client_ids=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        
+        this.setState({
+            user: res.data,
+            loading: false
+        })
+    }
+
     // clear Users
-    clearUsers = () => {
-        this.setState({ users: [], loading: false })
+    clearUsers = () => {        
+        window.location.reload();
+        this.setState({
+            loading: false
+        })
     }
 
     // set Alert 
@@ -88,7 +106,7 @@ class App extends Component {
     }
 
     render() {
-        const { users, user, loading } = this.state;
+        const { users, user, repos, loading } = this.state;
         return (
             <Router>
                 <div className='App'>
@@ -119,12 +137,15 @@ class App extends Component {
                             <Route exact path='/about' component={About}></Route>
                             <Route exact path='/user/:login' render={
                                 props => (
-                                    <User { ...props } getUser={this.getUser} user={user} loading={loading}></User>
+                                    <User { ...props } getUser={this.getUser}
+                                        getUserRepos={this.getUserRepos} 
+                                        user={user}
+                                        repos={repos}
+                                        loading={loading}>
+                                    </User>
                                 )                                
                             }></Route>
-                        </Switch>
-
-                        
+                        </Switch>                        
                     </div>        
                 </div>
             </Router>
